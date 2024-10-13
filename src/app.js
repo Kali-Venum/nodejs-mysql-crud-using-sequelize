0eba77058022b4dc2aa6ff30660dd6746b129e15
Kali-Venum <subhamdas.abcd@gmail.com>
2024-10-13 14:20:05 +0530

    working on app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`The server is running on port ${port}..!`);
-})
+  console.log(`The server is running on port ${port}`);
+});
