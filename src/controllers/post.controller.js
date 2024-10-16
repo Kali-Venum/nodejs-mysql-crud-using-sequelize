298decf06493d3dcf3e2f3e32a1fc5549a744450
Kali-Venum <subhamdas.abcd@gmail.com>
2024-10-16 23:54:54 +0530

    post create API done.

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,9 +1,22 @@
-function index(req, res) {
-  const posts = "Posts List";
+const models = require("../database/models");
 
-  res.send(posts);
+async function create(req, res) {
+  const post = {
+    title: req.body.title,
+    content: req.body.content,
+    image_url: req.body.imageURL,
+    category_id: req.body.categoryId,
+    user_id: req.body.userId,
+  };
+
+  const result = await models.Post.create(post);
+
+  res.status(201).json({
+    message: "A new post is created.",
+    data: result,
+  });
 }
 
 module.exports = {
-  index,
+  create,
 };
